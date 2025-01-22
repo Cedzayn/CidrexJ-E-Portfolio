9f00df452074c3f50c37c21f912f6398392a8b3f
Cedzayn <24-0325c@sgen.edu.ph>
2025-01-22 10:46:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/* ================ Typing Animation ================ */
+var typed = new Typed(".typing",
+    {
+        strings:["","IT Students","CyberSecurity","Web Developer"],
+        typeSpeed:100,
+        backSpeed: 60,
+        loop: true
+    }
+)
---
+++ style-switcher.js
@@ -0,0 +1,50 @@
+/* ======== Toggle Style Switcher ======= */
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+// hide-style - swticher on scroll
+window,addEventListener("scroll", () => {
+    if (document.querySelector(".style-switcher").classList.contains("open"))
+    {
+     document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+/* ======= theme colors ======= */
+// Select all alternate stylesheets
+const alternateStyles = document.querySelectorAll('.alternate-style');
+
+function setActiveStyle(color) {
+  alternateStyles.forEach((style) => {
+    if (color === style.getAttribute('title')) {
+      // Enable the selected stylesheet
+      style.removeAttribute('disabled');
+    } else {
+      // Disable all other stylesheets
+      style.setAttribute('disabled', 'true');
+    }
+  });
+}
+
+
+  
+/* ======= Dark and Light Mode Colors ======= */
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+    })
+
+window.addEventListener("load", () =>{
+    if (document.body.classList.contains("dark"))
+    {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else 
+    {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
+
+
